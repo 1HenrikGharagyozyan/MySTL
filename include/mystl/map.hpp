c7d1477ebf6e08f74b401135e0c8468e6181a25e
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-06-28 12:15:02 +0400

    feat(containers): implement associative map container based on RBTree
    - Integrated  wrapper around .
    - Enforced STL naming conventions (, , Missing filename ("less --help" for help)).
    - Implemented C++17 semantics including  and
      with perfect forwarding for lvalue and rvalue key types.
    - Added  nested class for algorithm compatibility.
    - Streamlined  lookup logic to prevent unnecessary allocations.
    - Secured iterators and capacity accessors with  and .

--- include/mystl/map.hpp
+++ include/mystl/map.hpp
@@ -3,17 +3,17 @@
 #include "rb_tree.hpp"
 #include "allocator.hpp"
 #include "utility.hpp"
+#include "functional.hpp" 
 
 #include <initializer_list>
 #include <stdexcept>
-#include <functional>
 
 namespace mystl 
 {
     template <
         typename Key, 
         typename T,
-        typename Compare = std::less<Key>, 
+        typename Compare = mystl::less, 
         typename Allocator = mystl::Allocator<mystl::Pair<const Key, T>>
     >
     class Map 
@@ -22,9 +22,22 @@ namespace mystl
         using key_type               = Key;
         using mapped_type            = T;
         using value_type             = mystl::Pair<const Key, T>;
+        using key_compare            = Compare;
         
+        class value_compare 
+        {
+            friend class Map;
+        protected:
+            Compare comp;
+            value_compare(Compare c) : comp(c) {}
+        public:
+            bool operator()(const value_type& lhs, const value_type& rhs) const 
+            {
+                return comp(lhs.first, rhs.first);
+            }
+        };
+
     private:
-        // Configure RBTree: Value = Pair<const Key, T>, extractor = Select1st
         using Tree = RBTree<Key, value_type, mystl::Select1st<value_type>, Compare, Allocator>;
         Tree tree_;
 
@@ -34,6 +47,8 @@ namespace mystl
         using allocator_type         = Allocator;
         using reference              = value_type&;
         using const_reference        = const value_type&;
+        using pointer                = typename mystl::allocator_traits<Allocator>::pointer;
+        using const_pointer          = typename mystl::allocator_traits<Allocator>::const_pointer;
         
         using iterator               = typename Tree::iterator;
         using const_iterator         = typename Tree::const_iterator;
@@ -43,6 +58,7 @@ namespace mystl
         // ========================================================================
         // CONSTRUCTORS
         // ========================================================================
+        
         Map() : tree_() {}
         
         explicit Map(const Compare& comp, const Allocator& alloc = Allocator()) 
@@ -75,13 +91,13 @@ namespace mystl
         }
 
         // ========================================================================
-        // RULE OF FIVE AND ALLOCATORS
+        // RULE OF FIVE
         // ========================================================================
-        Map(const Map& other) : tree_(other.tree_) {}
-        Map(const Map& other, const Allocator& alloc) : tree_(other.tree_, alloc) {}
         
+        Map(const Map& other) : tree_(other.tree_) {}
         Map(Map&& other) noexcept : tree_(mystl::move(other.tree_)) {}
-        Map(Map&& other, const Allocator& alloc) : tree_(mystl::move(other.tree_), alloc) {}
+        
+        ~Map() = default;
 
         Map& operator=(const Map& other) 
         { 
@@ -101,20 +117,19 @@ namespace mystl
         
         mapped_type& operator[](const key_type& key) 
         {
-            // OPTIMIZATION: First lookup. If the key already exists, don't allocate extra memory!
-            iterator it = tree_.find(key);
-            if (it != end()) 
-                return it->second;
-                
-            auto res = tree_.emplace_unique(key, T{});
-            return res.first->second;
+            return try_emplace(key).first->second;
+        }
+
+        mapped_type& operator[](key_type&& key) 
+        {
+            return try_emplace(mystl::move(key)).first->second;
         }
 
         mapped_type& at(const key_type& key) 
         {
             iterator it = tree_.find(key);
             if (it == end()) 
-                throw std::out_of_range("Map::at: key not found");
+                throw std::out_of_range("mystl::map::at: key not found");
             return it->second;
         }
 
@@ -122,22 +137,30 @@ namespace mystl
         {
             const_iterator it = tree_.find(key);
             if (it == cend()) 
-                throw std::out_of_range("Map::at: key not found");
+                throw std::out_of_range("mystl::map::at: key not found");
             return it->second;
         }
 
         // ========================================================================
-        // CONTAINER STATE
+        // CAPACITY & OBSERVERS
         // ========================================================================
+        
         [[nodiscard]] allocator_type get_allocator() const noexcept { return tree_.get_allocator(); }
         [[nodiscard]] bool empty() const noexcept { return tree_.empty(); }
         [[nodiscard]] size_type size() const noexcept { return tree_.size(); }
-        void clear() noexcept { tree_.clear(); }
+        
+        key_compare key_comp() const { return key_compare(); }
+        value_compare value_comp() const { return value_compare(key_comp()); }
 
+        // ========================================================================
+        // ITERATORS
+        // ========================================================================
+        
         iterator begin() noexcept { return tree_.begin(); }
         iterator end() noexcept { return tree_.end(); }
-        const_iterator begin() const noexcept { return tree_.cbegin(); }
-        const_iterator end() const noexcept { return tree_.cend(); }
+        const_iterator begin() const noexcept { return tree_.begin(); }
+        const_iterator end() const noexcept { return tree_.end(); }
+        
         const_iterator cbegin() const noexcept { return tree_.cbegin(); }
         const_iterator cend() const noexcept { return tree_.cend(); }
 
@@ -149,6 +172,9 @@ namespace mystl
         // ========================================================================
         // MODIFIERS
         // ========================================================================
+        
+        void clear() noexcept { tree_.clear(); }
+
         mystl::Pair<iterator, bool> insert(const value_type& value) 
         {
             return tree_.emplace_unique(value);
@@ -159,13 +185,25 @@ namespace mystl
             return tree_.emplace_unique(mystl::move(value));
         }
 
+        template <typename InputIt>
+        void insert(InputIt first, InputIt last) 
+        {
+            for (; first != last; ++first) {
+                tree_.emplace_unique(*first);
+            }
+        }
+
+        void insert(std::initializer_list<value_type> ilist) 
+        {
+            insert(ilist.begin(), ilist.end());
+        }
+
         template <typename... Args>
         mystl::Pair<iterator, bool> emplace(Args&&... args) 
         {
             return tree_.emplace_unique(mystl::forward<Args>(args)...);
         }
 
-        // C++17 try_emplace
         template <typename... Args>
         mystl::Pair<iterator, bool> try_emplace(const key_type& key, Args&&... args) 
         {
@@ -176,16 +214,25 @@ namespace mystl
             return tree_.emplace_unique(key, T(mystl::forward<Args>(args)...));
         }
 
-        // C++17 insert_or_assign (FIXED AND OPTIMIZED)
+        template <typename... Args>
+        mystl::Pair<iterator, bool> try_emplace(key_type&& key, Args&&... args) 
+        {
+            iterator it = tree_.find(key);
+            if (it != end()) 
+                return {it, false};
+            
+            return tree_.emplace_unique(mystl::move(key), T(mystl::forward<Args>(args)...));
+        }
+
         mystl::Pair<iterator, bool> insert_or_assign(const key_type& key, const T& obj) 
         {
             iterator it = tree_.find(key);
             if (it != end()) 
             {
-                it->second = obj; // If the key exists, safely overwrite the value
+                it->second = obj;
                 return {it, false};
             }
-            return tree_.emplace_unique(key, obj); // Otherwise insert a new node
+            return tree_.emplace_unique(key, obj);
         }
 
         mystl::Pair<iterator, bool> insert_or_assign(const key_type& key, T&& obj) 
@@ -193,10 +240,10 @@ namespace mystl
             iterator it = tree_.find(key);
             if (it != end()) 
             {
-                it->second = mystl::move(obj); // Safely move only if the key exists!
+                it->second = mystl::move(obj);
                 return {it, false};
             }
-            return tree_.emplace_unique(key, mystl::move(obj)); // Otherwise pass the move operation to the tree
+            return tree_.emplace_unique(key, mystl::move(obj));
         }
 
         size_type erase(const key_type& key) 
@@ -212,26 +259,41 @@ namespace mystl
         // ========================================================================
         // SEARCH
         // ========================================================================
-        iterator find(const key_type& key) { return tree_.find(key); }
-        const_iterator find(const key_type& key) const { return tree_.find(key); }
-
-        bool contains(const key_type& key) const { return tree_.contains(key); }
-
-        iterator lower_bound(const key_type& key) { return tree_.lower_bound(key); }
-        const_iterator lower_bound(const key_type& key) const { return tree_.lower_bound(key); }
-
-        iterator upper_bound(const key_type& key) { return tree_.upper_bound(key); }
-        const_iterator upper_bound(const key_type& key) const { return tree_.upper_bound(key); }
         
-        mystl::Pair<iterator, iterator> equal_range(const key_type& key) { return tree_.equal_range(key); }
-        mystl::Pair<const_iterator, const_iterator> equal_range(const key_type& key) const { return tree_.equal_range(key); }
+        iterator find(const key_type& key) noexcept { return tree_.find(key); }
+        const_iterator find(const key_type& key) const noexcept { return tree_.find(key); }
+
+        bool contains(const key_type& key) const noexcept { return tree_.contains(key); }
+
+        iterator lower_bound(const key_type& key) noexcept { return tree_.lower_bound(key); }
+        const_iterator lower_bound(const key_type& key) const noexcept { return tree_.lower_bound(key); }
+
+        iterator upper_bound(const key_type& key) noexcept { return tree_.upper_bound(key); }
+        const_iterator upper_bound(const key_type& key) const noexcept { return tree_.upper_bound(key); }
+        
+        mystl::Pair<iterator, iterator> equal_range(const key_type& key) noexcept { return tree_.equal_range(key); }
+        mystl::Pair<const_iterator, const_iterator> equal_range(const key_type& key) const noexcept { return tree_.equal_range(key); }
     };
 
-} // namespace mystl
+    template <typename Key, typename T, typename Compare, typename Allocator>
+    bool operator==(const Map<Key, T, Compare, Allocator>& lhs, 
+                    const Map<Key, T, Compare, Allocator>& rhs) 
+    {
+        return lhs.size() == rhs.size() && mystl::equal(lhs.begin(), lhs.end(), rhs.begin());
+    }
 
-// Support for allocator traits for Map
-namespace std
-{
-    template <typename Key, typename T, typename Compare, typename Alloc>
-    struct uses_allocator<mystl::Map<Key, T, Compare, Alloc>, Alloc> : true_type {};
-}
+    template <typename Key, typename T, typename Compare, typename Allocator>
+    bool operator!=(const Map<Key, T, Compare, Allocator>& lhs, 
+                    const Map<Key, T, Compare, Allocator>& rhs) 
+    {
+        return !(lhs == rhs);
+    }
+
+    template <typename Key, typename T, typename Compare, typename Allocator>
+    void swap(Map<Key, T, Compare, Allocator>& lhs, 
+              Map<Key, T, Compare, Allocator>& rhs) noexcept 
+    {
+        lhs.swap(rhs);
+    }
+
+} // namespace mystl
